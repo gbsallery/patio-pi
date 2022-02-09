03b072a9060746d6266314f4c54f850a40fa82d5
gsallery <gavin@sallery.co.uk>
2022-02-09 22:26:26 +0000

    Instantaneous animation override

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+#![feature(mutex_unlock)]
 
 #[macro_use] extern crate rocket;
 
@@ -24,8 +25,14 @@ fn off(animation: State<Arc<Mutex<Animation>>>) -> &'static str {
     let img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_fn(61, 1, |_x, _y| {
             image::Rgb([0u8,0u8,0u8])
     });
-    animation.lock().unwrap().img = img;
+    update_animation(animation, img);
     "Off"
+}
+
+fn update_animation(animation: State<Arc<Mutex<Animation>>>, img: ImageBuffer<Rgb<u8>, Vec<u8>>) {
+    let mut guard = animation.lock().unwrap();
+    guard.img = img;
+    guard.updated = true;
 }
 
 #[get("/on")]
@@ -33,7 +40,7 @@ fn on(animation: State<Arc<Mutex<Animation>>>) -> &'static str {
     let img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_fn(61, 1, |_x, _y| {
             image::Rgb([0x55u8,0x55u8,0x55u8])
     });
-    animation.lock().unwrap().img = img;
+    update_animation(animation, img);
     "On"
 }
 
@@ -45,7 +52,7 @@ fn solid(rgb: &RawStr, animation: State<Arc<Mutex<Animation>>>) -> Status {
     let img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_fn(61, 1, |_x, _y| {
             image::Rgb([r,g,b])
     });
-    animation.lock().unwrap().img = img;
+    update_animation(animation, img);
     Status::Ok
 }
 
@@ -53,7 +60,7 @@ fn solid(rgb: &RawStr, animation: State<Arc<Mutex<Animation>>>) -> Status {
 fn leds(animation: State<Arc<Mutex<Animation>>>) -> &'static str {
     let img = image::open("test.png").unwrap().to_rgb8();
 
-    animation.lock().unwrap().img = img;
+    update_animation(animation, img);
     "Flash!"
 }
 
@@ -62,32 +69,39 @@ fn image(data: Data, animation: State<Arc<Mutex<Animation>>>) -> &'static str {
     data.stream_to_file("/tmp/upload.png").map(|n| n.to_string()).unwrap();
 
     let img = image::open("/tmp/upload.png").unwrap().to_rgb8();
-    animation.lock().unwrap().img = img;
+    update_animation(animation, img);
     "Uploaded"
 }
 
-fn animate(mutex: Arc<Mutex<Animation>>) {
+fn animate(animation: Arc<Mutex<Animation>>) {
     loop {
-        let animation = mutex.lock().expect("Failed to acquire lock on animation").clone();
-        let image = animation.img;
-        let (width, height) = image.dimensions();
-        for r in 0..height {
-            let mut pixleds = Command::new("./rpi_pixleds");
-            pixleds.arg("-n");
-            pixleds.arg("61");
-            let mut pixels = String::new();
-            for n in (0..width).rev() {
-                let pixel = image.get_pixel(n, r);
-                pixels = pixels + &*format!("{:02X}", pixel[0]);
-                pixels = pixels + &*format!("{:02X}", pixel[1]);
-                pixels = pixels + &*format!("{:02X}", pixel[2]);
-                pixels = pixels + ",";
-            }
-            pixleds.arg(&pixels);
-            pixleds.output().expect("Failed to invoke rpi_pixleds");
+        loop {
+            let mut guard = animation.lock().expect("Failed to acquire lock on animation");
+            let image = guard.img.clone();
+            guard.updated = false;
+            Mutex::unlock(guard);
+            let (width, height) = image.dimensions();
+            for r in 0..height {
+                let mut pixleds = Command::new("./rpi_pixleds");
+                pixleds.arg("-n");
+                pixleds.arg("61");
+                let mut pixels = String::new();
+                for n in (0..width).rev() {
+                    let pixel = image.get_pixel(n, r);
+                    pixels = pixels + &*format!("{:02X}", pixel[0]);
+                    pixels = pixels + &*format!("{:02X}", pixel[1]);
+                    pixels = pixels + &*format!("{:02X}", pixel[2]);
+                    pixels = pixels + ",";
+                }
+                pixleds.arg(&pixels);
+                pixleds.output().expect("Failed to invoke rpi_pixleds");
 
-            let delay = time::Duration::from_millis(20);
-            thread::sleep(delay);
+                let guard = animation.lock().expect("Failed to acquire lock on animation");
+                if guard.updated { break };
+
+                let delay = time::Duration::from_millis(20);
+                thread::sleep(delay);
+            }
         }
     }
 }
@@ -95,7 +109,8 @@ fn animate(mutex: Arc<Mutex<Animation>>) {
 #[derive(Clone)]
 // struct Animation(Arc<Mutex<RgbImage>>);
 struct Animation {
-    img: RgbImage
+    img: RgbImage,
+    updated: bool
 }
 
 fn main() {
@@ -105,7 +120,7 @@ fn main() {
         image::Rgb([0u8,0u8,0u8])
     });
     // let animation = Animation(Arc::new(Mutex::new(img)));
-    let animation = Arc::new(Mutex::new(Animation{img}));
+    let animation = Arc::new(Mutex::new(Animation{img, updated: false }));
 
     let animation1 = animation.clone();
     std::thread::spawn(move || {
@@ -122,10 +137,16 @@ fn main() {
         .launch();
 }
 
-// TODO: Plan actual lighting scenes
-// TODO: scan a pixel, animated
 // TODO: Intensity clamp
-// TODO: Weather pattern
-// TODO: Progress bar API
-// TODO: Meater mode
 // TODO: Fade between states
+// TODO: scan a pixel, animated
+
+/*
+TODO move patterns to Neo:
+Dawn sunrise
+Opening up 8am
+Weather during day
+Sunset animation
+Random patterns until midnight
+Meater mode
+*/
